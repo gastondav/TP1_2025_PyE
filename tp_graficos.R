8acc7f41b2d0959c8afb6fddebfd2da3cbe73603
Matecapo <mateop1203@gmail.com>
2025-05-30 18:20:04 -0300

    acceso al agua: frecuencias a porcentaje, Cant abono: orden de variable, acceso a baño: grafico de torta

--- tp_graficos.R
+++ tp_graficos.R
@@ -30,7 +30,7 @@ colnames(datos_chico1) <- c("Provincia","Barrio","Integrantes en vivienda"
                             ,"Ambientes que se usan como dormitorios"
                             ,"Costo del alquiler"
                             ,"Tipo de acceso al agua"
-                            ,"Existencia de bano"
+                            ,"Existencia de baño"
                             ,"Tipo calefaccion 1"
                             ,"Tipo calefaccion 2"
                             ,"Tipo calefaccion 3"
@@ -61,11 +61,11 @@ datos_reducido1 %>% group_by(`Tipo de acceso al agua`) %>%filter(`Tipo de acceso
   ggplot() + 
   
   #aes(x = `Tipo de acceso al agua`) + # Frecuencias absolutas
-  aes(x = reorder(`Tipo de acceso al agua`, `Tipo de acceso al agua`, function(x) -length(x))) + # Ordenar según frecuencia
-  #aes(x = tiempo, y = ..count.. / sum(..count..)) + # Porcentajes
-  # aes(x = reorder(tiempo, tiempo, function(x) -length(x)), 
-  #		y = ..count.. / sum(..count..)) +  # Porcentajes ordenados según frecuencia
-  #scale_y_continuous(labels = scales::percent) +    # Eje para porcentajes
+  #aes(x = reorder(`Tipo de acceso al agua`, `Tipo de acceso al agua`, function(x) -length(x))) + # Ordenar según frecuencia
+  #aes(x = `Tipo de acceso al agua`, y = after_stat(count) / sum(after_stat(count))) + # Porcentajes
+  aes(x = reorder(`Tipo de acceso al agua`, `Tipo de acceso al agua`, function(x) -length(x)), 
+  y = after_stat(count) / sum(after_stat(count))) +  # Porcentajes ordenados según frecuencia
+  scale_y_continuous(labels = scales::percent) +    # Eje para porcentajes
   
   geom_bar(width = 0.75,   # Ancho de barras
            fill = '#CDAA7D',  # Color de relleno 
@@ -88,8 +88,8 @@ max(datos_reducido1$`Cant abono datos moviles`, na.rm = TRUE)
 datos_reducido1 %>% group_by(`Cant abono datos moviles`) %>% 
   ggplot() + 
   
-  #aes(x = `Tipo de acceso al agua`) + # Frecuencias absolutas
-  aes(x = reorder(`Cant abono datos moviles`, `Cant abono datos moviles`, function(x) -length(x))) + # Ordenar según frecuencia
+  aes(x = `Cant abono datos moviles`) + # Frecuencias absolutas
+  #aes(x = reorder(`Cant abono datos moviles`, `Cant abono datos moviles`, function(x) -length(x))) + # Ordenar según frecuencia
   #aes(x = tiempo, y = ..count.. / sum(..count..)) + # Porcentajes
   # aes(x = reorder(tiempo, tiempo, function(x) -length(x)), 
   #		y = ..count.. / sum(..count..)) +  # Porcentajes ordenados según frecuencia
@@ -108,3 +108,23 @@ datos_reducido1 %>% group_by(`Cant abono datos moviles`) %>%
  
 #La mediana 
 median(datos_reducido1$`Cant abono datos moviles`, na.rm = TRUE)
+
+#-----------------------------------------------------------------------------------------------
+
+#----------------------acceso a baño o letrina------------------------------------------------
+
+datos_reducido1 %>%
+  count(`Existencia de baño`) %>%
+  mutate(porcentaje = n / sum(n),
+         etiqueta = paste0( round(porcentaje * 100), "%")) %>%
+  ggplot() +
+  aes(x = "", y = porcentaje, fill = `Existencia de baño`)+
+  geom_col(width = 1,color='black') +                    # Barras apiladas
+  coord_polar(theta = "y") +              # Convertir a torta
+  geom_text(aes(label = etiqueta),
+            position = position_stack(vjust = 0.5)) +
+  ggtitle(" Acceso a baño o letrina en Barrios populares de CABA, año 2022") +
+  scale_fill_manual(values = c("#4E79A7",  # Azul fuerte y profesional
+                               "#F28E2B",  # Naranja cálido y alegre
+                               "#E15759"))+
+  theme_void()
